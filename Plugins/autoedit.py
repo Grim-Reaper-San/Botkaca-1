1d02af55a3868588ae656644326bf6ed87bdc17f
Yunoasta <90438201+Yunoasta@users.noreply.github.com>
2022-04-01 02:13:53 +0530

    Update autoedit.py

--- Plugins/autoedit.py
+++ Plugins/autoedit.py
@@ -15,35 +15,35 @@ caption_position = usercaption_position.lower()
 caption_text = Config.CAPTION_TEXT
 
 
-@autocaption.on_message(filters.channel & (filters.image | filters.audio ) & ~filters.edited, group=-1)
+@autocaption.on_message(filters.channel & (filters.image) & ~filters.edited, group=-1)
 async def editing(bot, message):
       try:
-         media = message.image or message.audio
+         media = message.image
          caption_text = Config.CAPTION_TEXT
       except:
          caption_text = ""
          pass 
-      if (message.image or message.audio): 
+      if (message.image): 
           if message.caption:                        
-             file_caption = f"**{message.caption}**"                
+             image_caption = f"**{message.caption}**"                
           else:
              fname = media.file_name
              filename = fname.replace("_", ".")
-             file_caption = f"`{filename}`"  
+             image_caption = f"`{filename}`"  
               
       try:
           if caption_position == "top":
              await bot.edit_message_caption(
                  chat_id = message.chat.id, 
                  message_id = message.message_id,
-                 caption = caption_text + "\n" + file_caption,
+                 caption = caption_text + "\n" + image_caption,
                  parse_mode = "markdown"
              )
           elif caption_position == "bottom":
              await bot.edit_message_caption(
                  chat_id = message.chat.id, 
                  message_id = message.message_id,
-                 caption = file_caption + "\n" + caption_text,
+                 caption = image_caption + "\n" + caption_text,
                  parse_mode = "markdown"
              )
           elif caption_position == "nil":
