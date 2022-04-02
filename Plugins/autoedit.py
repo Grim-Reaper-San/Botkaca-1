7f73da6dd754dcb98d8565382609d8290c4becd5
Yunoasta <90438201+Yunoasta@users.noreply.github.com>
2022-04-02 23:51:57 +0530

    Update autoedit.py

--- Plugins/autoedit.py
+++ Plugins/autoedit.py
@@ -43,7 +43,9 @@ async def editing(bot, message):
              await bot.edit_message_caption(
                  chat_id = message.chat.id, 
                  message_id = message.message_id,
-                 caption = file_caption + "\n" + caption_text,
+                 file_captions = jj
+                 jj = jj.replace(file_captions[-50:], "#anime")
+                 caption = jj + "\n" + caption_text,
                  parse_mode = "markdown"
              )
           elif caption_position == "nil":
