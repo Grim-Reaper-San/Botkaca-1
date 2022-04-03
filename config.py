e121280c7c71199a1effa9725f5576708e6b010e
Yunoasta <90438201+Yunoasta@users.noreply.github.com>
2022-04-03 14:35:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-      API_ID = int(os.environ.get("APP_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
-      CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "")
-      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
-      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "the_pirate_emperor")
+      BOT_TOKEN = "5274583191:AAHP3nn5fhqblYQEj4lZf-PGuBiXzLE3Plg"
+      API_ID = "3845818"
+      API_HASH = "95937bcf6bc0938f263fc7ad96959c6d"
+      CAPTION_TEXT = "@ANIMEXTLIVE"
+      CAPTION_POSITION = "BOTTOM"
+      ADMIN_USERNAME = "the_pirate_emperor"
