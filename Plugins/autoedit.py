a05f20add216ea463041e986c91ec036d57e050d
Yunoasta <90438201+Yunoasta@users.noreply.github.com>
2022-04-03 10:03:51 +0530

    Update autoedit.py

--- Plugins/autoedit.py
+++ Plugins/autoedit.py
@@ -46,7 +46,7 @@ async def editing(bot, message):
       newm_file_caption = newl_file_caption.replace("S3", "| Season 4")
       newn_file_caption = newm_file_caption.replace("S4", "| Season 4")
       newo_file_caption = newn_file_caption.replace("was released!!", "**")
-      newp_file_caption = newo_file_caption.seek(0)             # reposition the file pointer to the beginning of the file
+      newp_file_caption = newo_file_caption.seek(0)            
       newp_file_caption = newp_file_caption.write('**')
       try:
           if caption_position == "top":
