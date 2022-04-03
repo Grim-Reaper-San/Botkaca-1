23aca575e603ce650275f18b2292edb36987ef9c
Yunoasta <90438201+Yunoasta@users.noreply.github.com>
2022-04-03 22:56:44 +0530

    Update autoedit.py

--- Plugins/autoedit.py
+++ Plugins/autoedit.py
@@ -31,22 +31,22 @@ async def editing(bot, message):
              filename = fname.replace("_", ".")
              file_caption = f"`{filename}`"  
       new_file_caption = ' '.join(file_caption.split()[1:])  
-      newa_file_caption = new_file_caption.replace("- 0", "\n" + "🗓  Episode 0")  
-      newb_file_caption = newa_file_caption.replace("- 1", "\n" + "🗓  Episode 1") 
-      newc_file_caption = newb_file_caption.replace("- 2", "\n" + "🗓  Episode 2")
-      newd_file_caption = newc_file_caption.replace("- 3", "\n" + "🗓  Episode 3")
-      newe_file_caption = newd_file_caption.replace("- 4", "\n" + "🗓  Episode 4")
-      newf_file_caption = newe_file_caption.replace("- 5", "\n" + "🗓  Episode 5")
-      newg_file_caption = newf_file_caption.replace("- 6", "\n" + "🗓  Episode 6")
-      newh_file_caption = newg_file_caption.replace("- 7", "\n" + "🗓  Episode 7")
-      newi_file_caption = newh_file_caption.replace("- 8", "\n" + "🗓  Episode 8")
-      newj_file_caption = newi_file_caption.replace("- 9", "\n" + "🗓  Episode 9")
+      newa_file_caption = new_file_caption.replace("- 0", "\n" + "🗓 Episode 0")  
+      newb_file_caption = newa_file_caption.replace("- 1", "\n" + "🗓 Episode 1") 
+      newc_file_caption = newb_file_caption.replace("- 2", "\n" + "🗓 Episode 2")
+      newd_file_caption = newc_file_caption.replace("- 3", "\n" + "🗓 Episode 3")
+      newe_file_caption = newd_file_caption.replace("- 4", "\n" + "🗓 Episode 4")
+      newf_file_caption = newe_file_caption.replace("- 5", "\n" + "🗓 Episode 5")
+      newg_file_caption = newf_file_caption.replace("- 6", "\n" + "🗓 Episode 6")
+      newh_file_caption = newg_file_caption.replace("- 7", "\n" + "🗓 Episode 7")
+      newi_file_caption = newh_file_caption.replace("- 8", "\n" + "🗓 Episode 8")
+      newj_file_caption = newi_file_caption.replace("- 9", "\n" + "🗓 Episode 9")
       newk_file_caption = newj_file_caption.replace("S2", "Season 2")
       newl_file_caption = newk_file_caption.replace("S3", "Season 2")
       newm_file_caption = newl_file_caption.replace("S4", "Season 4")
       newn_file_caption = newm_file_caption.replace("S5", "Season 4")
       newo_file_caption = newn_file_caption.replace("was released!!", "**")        
-      newp_file_caption = f"**📺{newo_file_caption}"
+      newp_file_caption = f"**📺 {newo_file_caption}"
       
       try:
           if caption_position == "top":
