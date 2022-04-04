bf704662fcda2fabcce3221a28a1e970210518b5
Yunoasta <90438201+Yunoasta@users.noreply.github.com>
2022-04-05 01:55:08 +0530

    Update autoedit.py

--- Plugins/autoedit.py
+++ Plugins/autoedit.py
@@ -30,6 +30,8 @@ async def editing(bot, message):
              fname = media.file_name
              filename = fname.replace("_", ".")
              file_caption = f"**{filename}**"   
+      newo_file_caption = file_caption.replace("_", "")
+      newp_file_caption = newo_file_caption
       newa_file_caption = file_caption.replace("- 0", "\n" + "🗓 Episode 0")  
       newb_file_caption = newa_file_caption.replace("- 1", "\n" + "🗓 Episode 1") 
       newc_file_caption = newb_file_caption.replace("- 2", "\n" + "🗓 Episode 2")
@@ -56,7 +58,7 @@ async def editing(bot, message):
              await bot.edit_message_caption(
                  chat_id = message.chat.id, 
                  message_id = message.message_id,
-                 caption = f"📺 {newn_file_caption}" + "\n" + f"#{file_caption.replace(file_caption[-7:], "")}" + "\n" + f"**{caption_text}**",
+                 caption = f"📺 {newn_file_caption}" + "\n" + f"#{newp_file_caption.replace(newp_file_caption[-9:], "")}" + "\n" + f"**{caption_text}**",
                  parse_mode = "markdown"
              )
           elif caption_position == "nil":
