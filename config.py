b79cd807e0b9421bbfa354a33b0e791190267f13
Yunoasta <90438201+Yunoasta@users.noreply.github.com>
2022-04-05 03:12:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,25 @@ class Config(object):
       BOT_TOKEN = "5274583191:AAHP3nn5fhqblYQEj4lZf-PGuBiXzLE3Plg"
       API_ID = "3845818"
       API_HASH = "95937bcf6bc0938f263fc7ad96959c6d"
-      CAPTION_TEXT = "@ANIMEXT"
+      CAPTION_TEXT = "━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⚙️ Web-DL (Source)
+     📥 480p/720p/1080p   ➙ DOWNLOAD (http://t.me/animextlive)         
+
+⚙️ x264 encodes (SOFT-SUB)
+     📥 480p   ➙ [DOWNLOAD](http://t.me/animextlive)         
+     📥 720p   ➙ [DOWNLOAD](http://t.me/animextlive)          
+     📥 1080p ➙ [DOWNLOAD](http://t.me/animextlive)
+
+⚙️ x265 HEVC encodes (SOFT-SUB)
+     📥 480p   ➙ [DOWNLOAD](http://t.me/animextlive)         
+     📥 720p   ➙ [DOWNLOAD](http://t.me/animextlive)          
+     📥 1080p ➙ [DOWNLOAD](http://t.me/animextlive)
+
+⚙️ TG - Stream (HARD-SUB)
+     🖥 1080p ➙ [DOWNLOAD/STREAM](http://t.me/animextlive)
+━━━━━━━━━━━━━━━━━━━━━━━━━━━
+(Encoded Files will be uploaded as soon as it finishes encoding)
+━━━━━━━━━━━━━━━━━━━━━━━━━━━
+📡[Ongoing Anime](http://t.me/animextlive)"
       CAPTION_POSITION = "BOTTOM"
       ADMIN_USERNAME = "The_Pirate_Emperor"
