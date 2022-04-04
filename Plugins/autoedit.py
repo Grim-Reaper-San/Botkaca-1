e50e5b908ed30fff48b3f9bae31a26e2814e6fae
Yunoasta <90438201+Yunoasta@users.noreply.github.com>
2022-04-05 01:59:59 +0530

    Update autoedit.py

--- Plugins/autoedit.py
+++ Plugins/autoedit.py
@@ -25,12 +25,12 @@ async def editing(bot, message):
          pass 
       if (message.photo): 
           if message.caption:                        
-             file_caption = f"**{message.caption}**"                
+             muse_file_caption = f"**{message.caption}**"                
           else:
              fname = media.file_name
              filename = fname.replace("_", ".")
-             file_caption = f"**{filename}**"   
-      newo_file_caption = file_caption.replace(" ", "_")
+             fille_caption = f"**{filename}**"   
+      newo_file_caption = muse_file_caption.replace(" ", "_")
       newp_file_caption = newo_file_caption
       newa_file_caption = file_caption.replace("- 0", "\n" + "🗓 Episode 0")  
       newb_file_caption = newa_file_caption.replace("- 1", "\n" + "🗓 Episode 1") 
