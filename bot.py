8499b01a7baf3981eba748f4a275870bd0f5072f
Yunoasta <90438201+Yunoasta@users.noreply.github.com>
2022-04-25 22:36:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,9 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 class autocaption(Client):
-    
+    def __init__(self):
+        super().__init__(
+            session_name="Captioner",
             bot_token = Config.BOT_TOKEN,
             api_id = Config.API_ID,
             api_hash = Config.API_HASH,
