db22583974852cb36f02557ce94322c9a3ad0e84
Yunoasta <90438201+Yunoasta@users.noreply.github.com>
2022-04-26 11:20:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,30 @@
-from pyrogram import Client
+import pyrogram
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+
+from config import Config
+from pyrogram import Client 
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+class autocaption(Client):
+    
+    def __init__(self):
+        super().__init__(
+            session_name="Captioner",
             bot_token = Config.BOT_TOKEN,
+            api_id = Config.API_ID,
+            api_hash = Config.API_HASH,
             workers = 20,
             plugins = dict(
                 root="Plugins"
             )
-autocaption = Client(
-    "autocaptionistbot",
-    api_id=api_id, api_hash=api_hash,
-    bot_token=bot_token
-)
+        )
 
+if __name__ == "__main__" :
     autocaption().run()
+
