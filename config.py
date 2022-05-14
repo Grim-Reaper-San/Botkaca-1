09155379f99657784788cebef74915018e770a10
Grim-Reaper-San <103260040+Grim-Reaper-San@users.noreply.github.com>
2022-05-14 11:04:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,16 +6,6 @@ class Config(object):
       BOT_TOKEN = "5152810815:AAExPpIk0MuJafISDs-6zb3jH-Ig8ghKt90"
       API_ID = "3845818"
       API_HASH = "95937bcf6bc0938f263fc7ad96959c6d"
-      CAPTION_TEXT = "┏━━━━━━━━━━━━━━━━━━━━━┓
-┣ ⚙️ <b><i><u>Web-DL (Source)</u></i></b> 
-┃    ⁍ <a href=https://t.me/Airing_Anime_WebDL><b>480p / 720p / 1080p</b></a>
-┣ ⚙️ <b><i><u>x264 Encodes (SOFT-SUB)</u></i></b>
-┃    ⁍ <a href=https://t.me/Airing_Anime_480p><b>480p</b></a>      ⁍ <a href=https://t.me/Airing_Anime_720p><b>720p</b></a>      ⁍ <a href=https://t.me/Airing_Anime_1080p><b>1080p</b></a> 
-┣ ⚙️ <b><i><u>x265 HEVC Encodes (SOFT-SUB)</u></i></b>
-┃    ⁍ <a href=https://t.me/Airing_Anime_480p_x265><b>480p</b></a>      ⁍ <a href=https://t.me/Airing_Anime_720p_x265><b>720p</b></a>      ⁍ <a href=https://t.me/Airing_Anime_1080p_x265><b>1080p</b></a>
-┣ ⚙️ <b><i><u>x264 Encoded (HARD-SUB)</u></i></b> 
-┃    ⁍ <a href=https://t.me/Autoanime><b>TG - STREAM</b></a>
-┗━━━━━━━━━━━━━━━━━━━━━┛
- <b>☠️ @Latest_Ongoing_Airing_Anime ☠️</b>"
+      CAPTION_TEXT = "┏━━━━━━━━━━━━━━━━━━━━━┓\n┣ ⚙️ <b><i><u>Web-DL (Source)</u></i></b> \n┃    ⁍ <a href=https://t.me/Airing_Anime_WebDL><b>480p / 720p / 1080p</b></a>\n┣ ⚙️ <b><i><u>x264 Encodes (SOFT-SUB)</u></i></b>\n┃    ⁍ <a href=https://t.me/Airing_Anime_480p><b>480p</b></a>      ⁍ <a href=https://t.me/Airing_Anime_720p><b>720p</b></a>      ⁍ <a href=https://t.me/Airing_Anime_1080p><b>1080p</b></a> \n┣ ⚙️ <b><i><u>x265 HEVC Encodes (SOFT-SUB)</u></i></b>\n┃    ⁍ <a href=https://t.me/Airing_Anime_480p_x265><b>480p</b></a>      ⁍ <a href=https://t.me/Airing_Anime_720p_x265><b>720p</b></a>      ⁍ <a href=https://t.me/Airing_Anime_1080p_x265><b>1080p</b></a>\n┣ ⚙️ <b><i><u>x264 Encoded (HARD-SUB)</u></i></b> \n┃    ⁍ <a href=https://t.me/Autoanime><b>TG - STREAM</b></a>\n┗━━━━━━━━━━━━━━━━━━━━━┛\n <b>☠️ @Latest_Ongoing_Airing_Anime ☠️</b>"
       CAPTION_POSITION = "BOTTOM"
       ADMIN_USERNAME = "The_Pirate_Emperor"
