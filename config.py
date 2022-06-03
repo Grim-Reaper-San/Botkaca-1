7199f86f4cc4bad19901ea04de9475b59bb1acaa
Grim-Reaper-San <103260040+Grim-Reaper-San@users.noreply.github.com>
2022-06-03 19:25:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config(object):
       BOT_TOKEN = "5152810815:AAExPpIk0MuJafISDs-6zb3jH-Ig8ghKt90"
       API_ID = "3845818"
       API_HASH = "95937bcf6bc0938f263fc7ad96959c6d"
-      CAPTION_TEXT = "━━━━━━━━━━━━━━━━━━━━━━━━━\n<b><i>‣ Web-DL (Source)</i></b> ➙ <a href=https://t.me/Airing_Anime_WebDL><b><i>480p/720p/1080p</b></i></a>\n<b><i>‣ x264 Encoded (HARD-SUB)</i></b> ➙ <a href=https://t.me/Autoanime><b><i>WATCH</b></i></a>\n━━━━━━━━━━━━━━━━━━━━━━━━━\n <b>「 @Latest_Ongoing_Airing_Anime 」</b>"
+      CAPTION_TEXT = "━━━━━━━━━━━━━━━━━━━━━━━━━\n<b><i>‣ Web-DL (Source)</i></b> ➙ <a href=https://t.me/Airing_Anime_WebDL><b><i>480p/720p/1080p</b></i></a>\n<b><i>\n━━━━━━━━━━━━━━━━━━━━━━━━━\n <b>「 @Latest_Ongoing_Airing_Anime 」</b>"
       CAPTION_POSITION = "BOTTOM"
       ADMIN_USERNAME = "The_Pirate_Emperor"
